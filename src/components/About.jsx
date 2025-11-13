a4b576b34556a8c229cfe8c0595960d4ed3d1cc0
JannatMollah <jannatmollah@gmail.com>
2025-11-13 13:38:38 +0600

    About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About The Book Haven</h2>
+        <div className='w-11/12 mx-auto'>
+            <h2 className="text-4xl text-center font-bold mb-6"> About Book Haven</h2>
+            <div className='bg-white flex items-center justify-center rounded-xl h-20'>
+                <p className='text-2xl italic'>Book Haven is world largest and famous digital library. It store all kinds of books. Reader can read there books in our site</p>
+            </div>
         </div>
     );
 };
--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Genres = () => {
     return (
-        <div className='w-11/12 mx-auto'>
+        <div>
             
         </div>
     );
